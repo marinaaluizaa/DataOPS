ac2ac29c9b7253a858dcfe1bbe0e172fec09e9ea
MarinaLuiza99 <marinabirck30@gmail.com>
2024-10-10 20:10:07 -0300

    1.3. Salvar Pandas Dataframe no MongoDB

--- carros_mont.py
+++ carros_mont.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pymongo import MongoClient
 
 # Dados dos carros
 dados_carros = {
@@ -17,10 +18,24 @@ dados_montadora = {
 
 data_montadora = pd.DataFrame(dados_montadora)
 
-# Exibe os dados dos carros em formato de DataFrame
-print("Carros:")
-print(data_carros)
+# dados dos carros
+# print("Carros:")
+# print(data_carros)
 
-# Exibe os dados das montadoras em formato de DataFrame
-print("\nMontadora:")
-print(data_montadora)
+# dados das montadoras 
+# print("\nMontadora:")
+# print(data_montadora)
+
+client = MongoClient('mongodb://localhost:27017/')  # URI padrão do MongoDB local
+db = client['local'] 
+
+carros_dict = data_carros.to_dict('records')
+montadoras_dict = data_montadora.to_dict('records')
+
+# insere os dados na collection carros
+carros_collection = db['Carros']
+carros_collection.insert_many(carros_dict)
+
+# insere os dados na collection montadoras
+montadoras_collection = db['Montadoras']
+montadoras_collection.insert_many(montadoras_dict)
